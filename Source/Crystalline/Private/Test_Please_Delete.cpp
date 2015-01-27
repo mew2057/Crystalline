08dbd825a4fb6d25ea4435af8c9aa8c8b0bd8b81
Nick Buonarota <NickyDaB@gmail.com>
2015-01-27 16:02:36 -0500

    Testing new cpp and h files. Removed .sln from tracking.
    Former-commit-id: c64b4163a2e4152ebb89534c904e07809e54ad15

---
+++ Source/Crystalline/Private/Test_Please_Delete.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Crystalline.h"
+#include "Test_Please_Delete.h"
+
+Test_Please_Delete::Test_Please_Delete()
+{
+}
+
+Test_Please_Delete::~Test_Please_Delete()
+{
+}
---
+++ Source/Crystalline/Public/Test_Please_Delete.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class CRYSTALLINE_API Test_Please_Delete
+{
+public:
+	Test_Please_Delete();
+	~Test_Please_Delete();
+};
