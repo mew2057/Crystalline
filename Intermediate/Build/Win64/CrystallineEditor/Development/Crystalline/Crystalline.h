aad79c8ba41f5aa4fc401caaa8b66c71ca319c3d
John Dunham <JFDBear>
2015-01-27 11:29:29 -0500

    git ignores in place VS2013 now builds.
    Former-commit-id: ee14f701d141a046dc49aaf992ee20cfcb5a9b05

--- Intermediate/Build/Win64/CrystallineEditor/Development/Crystalline/Crystalline.h
+++
@@ -1,5 +0,0 @@
-#ifndef __AUTO_CRYSTALLINE_H__
-#define __AUTO_CRYSTALLINE_H__
-//Last Write: 1/26/2015 9:31:56 PM +00:00
-#include "D:\Users\jd8060\Capstone Unreal\Crystalline\Source\Crystalline\Public\Crystalline.h"
-#endif//__AUTO_CRYSTALLINE_H__
--- Intermediate/Build/Win64/CrystallineEditor/Development/Crystalline/PCH.Crystalline.Crystalline.h.cpp
+++
@@ -1,1 +0,0 @@
-#include "D:\Users\jd8060\Capstone Unreal\Crystalline\Source\Crystalline\Public\Crystalline.h"
