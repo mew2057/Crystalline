33c9465a03b225b2c26f34ef108900ef7a80fe4d
John Dunham <JFDBEar1@gmail.com>
2015-02-13 13:17:50 -0500

    Added stubs for all of the currently "designed" weapons.
    Former-commit-id: a08ec6091b57c069afee63c6f717b562408a2b96

---
+++ Source/Crystalline/Weapons/CGShotgun.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Weapons/CGCrystalGun.h"
+#include "CGShotgun.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRYSTALLINE_API ACGShotgun : public ACGCrystalGun
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
