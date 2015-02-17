d59013a7a6558c39e7092e225d39320390eff290
John Dunham <JFDBEar1@gmail.com>
2015-02-16 22:45:28 -0500

    Issue #30 Added some grey boxed code for the Crystal Pickup. Next step is making the crystal display pickup message.
    Former-commit-id: 7a8ff314ab067890102812e4226bb4bcc0d31171

--- Source/Crystalline/Pickups/CGCrystal.cpp
+++ Source/Crystalline/Pickups/CGCrystal.cpp
@@ -6,7 +6,44 @@
 
 ACGCrystal::ACGCrystal(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
+	
+	OverlapVolume = ObjectInitializer.CreateDefaultSubobject<UCapsuleComponent>(this, TEXT("CrystalOverlapVolume"));
+	OverlapVolume->SetCapsuleHalfHeight(88.f);
+	OverlapVolume->SetCapsuleRadius(80.f);
+	RootComponent = OverlapVolume;
+
+	CrystalMesh = ObjectInitializer.CreateDefaultSubobject<UStaticMeshComponent>(this, TEXT("CrystalMesh"));
+	CrystalMesh->bReceivesDecals = false;
+	CrystalMesh->CastShadow = true;
+	CrystalMesh->bOnlyOwnerSee = false;
+	CrystalMesh->bOwnerNoSee = false;
+	CrystalMesh->SetCollisionObjectType(ECC_WorldStatic);
+	CrystalMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	CrystalMesh->SetCollisionResponseToAllChannels(ECR_Ignore);
+	CrystalMesh->SetHiddenInGame(false);
+	CrystalMesh->RelativeLocation = FVector(0.f, 0.f, 50.f);
+	CrystalMesh->AttachParent = OverlapVolume;
+	
+	BaseMesh = ObjectInitializer.CreateDefaultSubobject<UStaticMeshComponent>(this, TEXT("CrystalBaseMesh"));
+	BaseMesh->bReceivesDecals = false;                             
+	BaseMesh->CastShadow = true;                                   
+	BaseMesh->bOnlyOwnerSee = false;
+	BaseMesh->bOwnerNoSee = false;
+	BaseMesh->SetCollisionObjectType(ECC_WorldStatic);			   
+	BaseMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision); 
+	BaseMesh->SetCollisionResponseToAllChannels(ECR_Ignore);       
+	BaseMesh->SetHiddenInGame(false);
+	CrystalMesh->RelativeLocation = FVector(0.f, 0.f, -80.f);
+	BaseMesh->AttachParent = OverlapVolume;
+	
 	//TODO Stuff
+	CrystalType = ECrystalType::UPGRADE;
+
+
+	// Make it so we have replication.
+	bReplicates = true;
+	bReplicateInstigator = true;
+	bNetUseOwnerRelevancy = true;
 }
 
 
@@ -14,5 +51,65 @@ void ACGCrystal::ReceiveActorBeginOverlap(class AActor* Other)
 {
 	Super::ReceiveActorBeginOverlap(Other);
 	// TODO Tell the player that they can pickup the item.
+	ACGCharacter* Player = Cast<ACGCharacter>(Other);
+	if (Player)
+	{
+		UE_LOG(LogTemp, Log, TEXT("Crystal is colliding with a player. "));
+	}
+	UE_LOG(LogTemp, Log, TEXT("OverLap. "));
+
+}
+
+void ACGCrystal::ReceiveActorEndOverlap(class AActor* Other)
+{
+	Super::ReceiveActorEndOverlap(Other);
+
+	ACGCharacter* Player = Cast<ACGCharacter>(Other);
+	if (Player)
+	{
+		UE_LOG(LogTemp, Log, TEXT("Crystal is no longer colliding with a player. "));
+	}
+}
+
+bool ACGCrystal::Pickup()
+{
+	if (!bIsActive)
+	{
+		return false;
+	}
+
+	// Hide the crystal.
+	HideCrystal();
+
+	return true;
+}
+
+void ACGCrystal::HideCrystal()
+{
+	if (TimeToRespawn <= 0)
+	{ 
+		return;
+	}
+
+	GetWorldTimerManager().SetTimer(this, &ACGCrystal::ShowCrystal, TimeToRespawn, false);
+}
+
+void ACGCrystal::ShowCrystal()
+{
+
+}
+
+
+
+void ACGCrystal::OnRep_Active()
+{
+
+}
+
+void ACGCrystal::GetLifetimeReplicatedProps(TArray< FLifetimeProperty > & OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ACGCrystal, bIsActive);
 }
 
--- Source/Crystalline/Pickups/CGCrystal.h
+++ Source/Crystalline/Pickups/CGCrystal.h
@@ -5,6 +5,17 @@
 #include "GameFramework/Actor.h"
 #include "CGCrystal.generated.h"
 
+
+UENUM(BlueprintType)		
+enum class ECrystalType : uint8
+{
+	UPGRADE UMETA(DisplayName = "Upgrade"),
+	FORCE UMETA(DisplayName = "Force"),
+	ACCURACY  UMETA(DisplayName = "Accuracy"),
+	UTILITY	 UMETA(DisplayName = "Utility")
+};
+
+
 /**
  * 
  */
@@ -15,6 +26,51 @@ class CRYSTALLINE_API ACGCrystal : public AActor
 public:
 	ACGCrystal(const FObjectInitializer& ObjectInitializer);
 
-	/** Triggers the pickup prompt for the crystal, if the crystal gun is equipped.*/
+	/** Triggers the pickup prompt for the crystal. */
 	virtual void ACGCrystal::ReceiveActorBeginOverlap(class AActor* Other) override;
+	
+	/** Removes the pickup prompt for the crystal. */
+	virtual void ACGCrystal::ReceiveActorEndOverlap(class AActor* Other) override;
+
+	/** Invoked by the actor, returns false if the pickup is invalid.*/
+	bool Pickup();
+
+	/**Hides the crystal and sets it's state to inactive. */
+	void HideCrystal();
+
+	/** Shows the crystal and sets it's state to active. */
+	void ShowCrystal();
+
+	UFUNCTION()
+	void OnRep_Active();
+
+
+protected:
+	/** Crystal Mesh, defines the Noteworthy portion of the crystal.*/
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	UMeshComponent* CrystalMesh;
+
+	/** Base Mesh, defines Base that the crystal is on (if any).*/
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	UMeshComponent* BaseMesh;
+
+	/**The overlap volume, if the player collides with this they may grab the crystal. */
+	UPROPERTY(VisibleDefaultsOnly, Category = Config)
+	UCapsuleComponent* OverlapVolume;
+
+	/** The Crystal type of the pickup. */
+	UPROPERTY(EditDefaultsOnly, Category = Config)
+	ECrystalType CrystalType;
+	
+	/** Tracks whether or not the crystal can be picked up. */
+	UPROPERTY(Transient, ReplicatedUsing=OnRep_Active)
+	uint32 bIsActive : 1;
+
+	/**If set to true the crystal will spawn as though it were present on the construction of the object. */
+	UPROPERTY(EditDefaultsOnly, Category = Config)
+	uint32 bSpawnActive : 1;
+
+	/** Time to respawn after pickup, if set to zero the crystal is never exhausted. */
+	UPROPERTY(EditDefaultsOnly, Category = Config)
+	float TimeToRespawn;
 };
