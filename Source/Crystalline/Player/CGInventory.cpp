6dc09cccb6c937ace425a66f5846ded043c07bbb
John Dunham <JFDBear>
2015-02-18 18:27:47 -0500

    Removed an ammo import.
    Former-commit-id: c90b5c3ac93c7ce64b435faecc9408c3b16dfe90

--- Source/Crystalline/Player/CGInventory.cpp
+++ Source/Crystalline/Player/CGInventory.cpp
@@ -1,7 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Crystalline.h"
-#include "Weapons/CGAmmo.h"
 #include "CGInventory.h"
 
 ACGInventory::ACGInventory(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
