dd1db1e37198e0952a3caa3cf2d3bc6cd8816add
John Dunham <JFDBear>
2015-03-09 16:03:38 -0400

    Added game settings and game save stubs.
    Former-commit-id: e319c6eda0a6c160ff907b9b67d2cf00a10f9c95

---
+++ Source/Crystalline/Misc/CGGameSettings.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Crystalline.h"
+#include "CGGameSettings.h"
+
+
+UCGGameSettings::UCGGameSettings(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+
+}
+
+
