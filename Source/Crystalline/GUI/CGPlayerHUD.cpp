8030dcfa5e8dee31b9a63938cedae56a710c94d2
John Dunham <JFDBEar1@gmail.com>
2015-03-10 21:48:59 -0400

    Finished all of the Core Weapon Crosshairs.
    Former-commit-id: 61214d471ada3f4b9ef967a45dd14596c69b0494

--- Source/Crystalline/GUI/CGPlayerHUD.cpp
+++ Source/Crystalline/GUI/CGPlayerHUD.cpp
@@ -166,7 +166,8 @@ void ACGPlayerHUD::DrawHUD()
 			// Crosshair	
 			if (!bScoreboardVisible)
 			{
-				Canvas->SetDrawColor(Weapon->CheckCanHit() ? FColor::Red : FColor::White);
+				bool bIsHitting = GetWorld()->GetTimeSeconds() - TimeSinceLastHitConfirmed < TimeToDisplayHitConfirmed;
+				Canvas->SetDrawColor(bIsHitting || Weapon->CheckCanHit() ? FColor::Red : FColor::White);
 
 				FCanvasIcon CrosshairIcon = Weapon->WeaponHUDConfig.CrosshairIcon;
 				// ScaleUI is 1 at 1080, .5 at 540 and 2 at 2160.
@@ -176,7 +177,7 @@ void ACGPlayerHUD::DrawHUD()
 					(Center.Y - (CrosshairIcon.VL * ScaleUIY * 0.5f)), ScaleUIY);
 
 				// Play hit confirmation
-				if (GetWorld()->GetTimeSeconds() - TimeSinceLastHitConfirmed < TimeToDisplayHitConfirmed)
+				if (bIsHitting)
 				{
 					//Canvas->SetDrawColor(FLinearColor(1.f, 1.f, 1.f, 1 - (GetWorld()->GetTimeSeconds() - TimeSinceLastHitConfirmed) / TimeToDisplayHitConfirmed));
 
