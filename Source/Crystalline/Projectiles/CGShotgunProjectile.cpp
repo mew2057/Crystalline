2c029f325060802d4819d6ad2525286491040761
John Dunham <JFDBear>
2015-03-19 20:27:20 -0400

    Working on improving the crosshairs based on internal testing, starting with the shotgun. Added a Dropoff factor to the shotgun.
    Former-commit-id: d61b67d52666749fc0d67b30ffddf239c20eed94

--- Source/Crystalline/Projectiles/CGShotgunProjectile.cpp
+++ Source/Crystalline/Projectiles/CGShotgunProjectile.cpp
@@ -3,18 +3,36 @@
 #include "Crystalline.h"
 #include "CGShotgunProjectile.h"
 
-
-
-
 ACGShotgunProjectile::ACGShotgunProjectile(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
-	
+	EffectiveDistance = 1250.f;
+	OutOfRangeModifier = .25f;
 }
 
-
-void ACGShotgunProjectile::PostInitializeComponents()
+void ACGShotgunProjectile::ProcessImpact(const FHitResult& Hit)
 {
-	Super::PostInitializeComponents();
+	if (Hit.GetActor())
+	{
 
+		FPointDamageEvent PointDmg;
+		PointDmg.DamageTypeClass = DamageType;
+		PointDmg.HitInfo = Hit;
+		PointDmg.ShotDirection = Hit.ImpactNormal;
+
+		// Hard drop off for damage at the extent of the effective range.
+		const float EstDistTravelled = MovementComp->InitialSpeed * (GetWorld()->GetTimeSeconds() - CreationTime);
+		PointDmg.Damage = ImpactDamage * (EstDistTravelled < EffectiveDistance ? 1 : OutOfRangeModifier);
+
+		Hit.GetActor()->TakeDamage(PointDmg.Damage, PointDmg, GetInstigatorController(), this);
+	}
+
+	// TODO  Add Impulse!
+	// Only add impulse and destroy projectile if we hit a physics
+	/*if ((OtherActor != NULL) && (OtherActor != this) && (OtherComp != NULL) && OtherComp->IsSimulatingPhysics())
+	{
+	OtherComp->AddImpulseAtLocation(GetVelocity() * 100.0f, GetActorLocation());
+
+
+	}*/
 }
